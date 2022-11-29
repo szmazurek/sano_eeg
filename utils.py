46705ec3cfeb74fe6bec7b6756925802ec4ca716
Szymon Mazurek <szymonmazurek57@gmail.com>
2022-11-29 14:51:54 +0100

    Utils updates

--- utils.py
+++ utils.py
@@ -127,13 +127,15 @@ def run_preprocessing(
     freq_l=1.0,
     freq_h=30.0,
     avg_ref=True,
-    apply_pca = True,
+    apply_pca=True,
     apply_ica=False,
     informax=False,
 ):
     """Runs preprocessing on given mne.raw instance."""
     if apply_ica and apply_pca:
-        raise ValueError("Values of apply_pca and apply_ica cannot both be True! Choose only one method of artifact removal.")
+        raise ValueError(
+            "Values of apply_pca and apply_ica cannot both be True! Choose only one method of artifact removal."
+        )
 
     raw.load_data()
     raw.filter(l_freq=freq_l, h_freq=freq_h, h_trans_bandwidth=1)
@@ -155,14 +157,14 @@ def run_preprocessing(
         labels = np.array(ic_labels["labels"])
         ica.exclude = np.where((labels != "other") & (labels != "brain"))[0].tolist()
         ica.apply(raw)
-        
+
     elif apply_pca:
         pca = PCA()
         pca.fit(raw._data)
         components = pca.components_
         mu = pca.mean_
         transform_raw = pca.transform(raw._data)
-        raw._data = np.squeeze(mu + transform_raw[:,2:] @ components[2:])
+        raw._data = np.squeeze(mu + transform_raw[:, 2:] @ components[2:])
 
     return raw
 
@@ -200,7 +202,9 @@ def load_and_dump_channels(filepath):
     return data_raw
 
 
-def preprocess_dataset(subjects_with_seizures_path, dataset_dirpath, preprocessed_dirpath):
+def preprocess_dataset(
+    subjects_with_seizures_path, dataset_dirpath, preprocessed_dirpath
+):
     """Runs full preprocessing on the dataset cointained in the folder.
     Args:  
         subject_with_seizures_path: path to a text file containing recordings with seizures.
@@ -211,7 +215,9 @@ def preprocess_dataset(subjects_with_seizures_path, dataset_dirpath, preprocesse
         preprocessed_dirpath: path to folder in which preprocessed files will be saved.
 
     """
-    subjects_with_seizures = [subject[:-1] for subject in open(subjects_with_seizures_path,'r').readlines()]
+    subjects_with_seizures = [
+        subject[:-1] for subject in open(subjects_with_seizures_path, "r").readlines()
+    ]
     for subject in subjects_with_seizures:
         try:
             subject_path = os.path.join(dataset_dirpath, subject)
@@ -221,10 +227,10 @@ def preprocess_dataset(subjects_with_seizures_path, dataset_dirpath, preprocesse
         except:
             print(f"Subject {subject} not found.")
             continue
-        
+
         reorder_channels_chbmit(raw_file)
-        
-        raw_instance = run_preprocessing(raw_file, apply_pca=True,freq_l=2)
+
+        raw_instance = run_preprocessing(raw_file, apply_pca=True, freq_l=2)
         save_path = os.path.join(preprocessed_dirpath, subject)
         if not os.path.exists(os.path.split(save_path)[0]):
             os.mkdir(os.path.split(save_path)[0])
@@ -237,7 +243,7 @@ def prepare_timestep_array(array, timestep, overlap):
     into [samples_count,n_nodes,feature_per_node,timestep]."""
     features = [
         array[:, :, i : i + timestep]
-        for i in range(0, array.shape[2] - timestep, timestep - overlap)
+        for i in range(0, array.shape[2] - timestep + 1, timestep - overlap)
     ]
     return np.array(features)
 
@@ -258,23 +264,24 @@ def extract_training_data_and_labels(
     start_ev_array,
     stop_ev_array,
     savepath=None,
-    seizure_lookback=600,
-    sample_timestep=10,
-    overlap=9,
+    seizure_lookback: int = 600,
+    sample_timestep: int = 10,
+    overlap: int = 9,
 ):
     ## TODO - skończyć to i przetestować bo źle to wygląda, dorobić zapis
     """Function to extract seizure periods and preictal perdiods into samples ready to be put into graph neural network."""
     for n, start_ev in enumerate(start_ev_array):
-        fs = raw.info["sfreq"]
-        seizure_lookback = fs * seizure_lookback
+        fs = int(raw.info["sfreq"])
+        seizure_lookback = seizure_lookback
         prev_event_time = start_ev - stop_ev_array[n - 1] if n > 0 else start_ev
+
         if prev_event_time > seizure_lookback:
             interictal_period = raw.get_data(
-                start=start_ev - seizure_lookback, stop=start_ev
+                start=(start_ev - seizure_lookback) * fs, stop=start_ev * fs
             )
         else:
             interictal_period = raw.get_data(
-                start=start_ev - prev_event_time, stop=start_ev
+                start=(start_ev - prev_event_time) * fs, stop=start_ev * fs
             )
 
         interictal_period = (
@@ -291,28 +298,59 @@ def extract_training_data_and_labels(
         interictal_event_time_labels = prepare_timestep_label(
             interictal_period, sample_timestep * fs, overlap * fs
         )  ## assign time to seizure for every sample [s]
-        seizure_period = raw.get_data(start=start_ev, stop=stop_ev_array[n])
+        seizure_period = raw.get_data(start=start_ev * fs, stop=stop_ev_array[n] * fs)
+        print(seizure_period.shape)
         seizure_period = (
             np.expand_dims(seizure_period.transpose(), axis=2)
             .swapaxes(0, 2)
             .swapaxes(0, 1)
         )
+        print(seizure_period.shape)
         seizure_features = prepare_timestep_array(
             array=seizure_period, timestep=sample_timestep * fs, overlap=overlap * fs
         )
-        seizure_event_labels = np.ones(
-            seizure_features.shape[0]
-        )  ## assign label 1 to every seizure period sample
+        seizure_event_labels = np.ones(seizure_features.shape[0])
+        if n == 0:
+            full_interictal_features = interictal_features
+            full_interictal_event_labels = interictal_event_labels
+            full_interictal_event_time_labels = interictal_event_time_labels
+            full_seizure_features = seizure_features
+            full_seizure_event_labels = seizure_event_labels
+        else:
+            full_interictal_features = np.concatenate(
+                (full_interictal_features, interictal_features)
+            )
+            full_interictal_event_labels = np.concatenate(
+                (full_interictal_event_labels, interictal_event_labels)
+            )
+            full_interictal_event_time_labels = np.concatenate(
+                (full_interictal_event_time_labels, interictal_event_time_labels)
+            )
+            full_seizure_features = np.concatenate(
+                (full_seizure_features, seizure_features)
+            )
+            full_seizure_event_labels = np.concatenate(
+                (full_seizure_event_labels, seizure_event_labels)
+            )
+        
+        ## assign label 1 to every seizure period sample
         # savepath_interictal_data = f"{savepath}_interictal_{n}"
         # savepath_interictal_labels_e = f"{savepath}_interictal_labels_e_{n}"
         # savepath_interictal_labels_t = f"{savepath}_interictal_labels_t_{n}"
         # savepath_ictal_data = f"{savepath}_ictal_{n}"
         # savepath_ictal_labels_e = f"{savepath}_ictal_labels_e_{n}"
         # np.save()
-    return interictal_features, interictal_event_labels, interictal_event_time_labels
+    return (
+        full_interictal_features,
+        full_interictal_event_labels,
+        full_interictal_event_time_labels,
+        full_seizure_features,
+        full_seizure_event_labels,
+    )
 
-def run_prep(raw,line_freq,ransac = False, channel_wise = False):
-    sfreq = raw.info['sfreq']
+
+def run_prep(raw, line_freq, ransac=False, channel_wise=False):
+    sfreq = raw.info["sfreq"]
     prep_params = {
         "ref_chs": "eeg",
         "reref_chs": "eeg",
@@ -320,6 +358,8 @@ def run_prep(raw,line_freq,ransac = False, channel_wise = False):
     }
     raw.load_data()
     montage = raw.get_montage()
-    prep = PrepPipeline(raw, prep_params,montage,ransac=ransac,channel_wise=channel_wise)
+    prep = PrepPipeline(
+        raw, prep_params, montage, ransac=ransac, channel_wise=channel_wise
+    )
     prep.fit()
     return prep
